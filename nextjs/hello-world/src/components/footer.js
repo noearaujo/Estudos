c412c4a5a708ed524762fa8914da00be46527e79
noearaujo <araujonoe21@hotmail.com>
2023-03-19 04:06:39 -0300

    Layout Components

---
+++ nextjs/hello-world/src/components/footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <p>Next.js Estudos &copy; 2023</p>
+    )
+}
--- nextjs/hello-world/src/pages/_app.js
+++ nextjs/hello-world/src/pages/_app.js
@@ -1,11 +1,11 @@
-import Navbar from '@/components/navbar'
+
+import Layout from '@/components/layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Navbar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   )
 }
