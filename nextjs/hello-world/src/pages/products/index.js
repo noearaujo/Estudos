5f1c4cf11f9b6eca6ca03db05401f5fc925e6ee3
noearaujo <araujonoe21@hotmail.com>
2023-03-19 02:41:11 -0300

    Components and Link

--- nextjs/hello-world/src/pages/products/index.js
+++ nextjs/hello-world/src/pages/products/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link"
+
 export default function Products() {
     return (
-        <h1>Products</h1>
+        <>
+            <h1>Products</h1>
+            <ul>
+                <li><Link href="/products/pants/bluepant">Blue Pant</Link></li>
+                <li><Link href="/products/pants/redpant">Red Pant</Link></li>
+                <li><Link href="/products/shirt">Shirt</Link></li>
+            </ul>
+        </>
     )
 }
