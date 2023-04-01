0d6f34b88e8c5e07b8cb2d4e46824250a9795547
noearaujo <araujonoe21@hotmail.com>
2023-04-01 08:00:00 -0300

    rotas dinamicas StaicPaths e StaticProps

--- nextjs/hello-world/src/pages/todos/[todoid].js
+++ nextjs/hello-world/src/pages/todos/[todoid].js
@@ -1,22 +1,47 @@
 import Link from "next/link";
-import {useRouter} from "next/router";
+import Router from "next/router";
 
-export default function Todo() {
-    const router = useRouter()
-    const todoId = router.query.todoid
+export async function getStaticProps(context) {
+    const {params} = context
+    const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.todoid}`)
+    const todo = await data.json()
+
+    return {
+        props: {todo}
+    }
+}
+
+export async function getStaticPaths() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    const paths = data.map((todo) => {
+        return {
+            params: {
+                todoid: `${todo.id}`
+            }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default function Todo({todo}) {
     return (
         <>
-            <button onClick={() => router.back()}>Voltar</button>
-            <h1>Exibindo Todo: {todoId}</h1>
-            <br />
+            <button onClick={() => Router.back()}>Voltar</button>
+            <h1>Exibindo Todo: {todo.id}</h1>
+            <h3>{todo.title}</h3>
             <br />
             <br />
             <p>Comentarios:<br /><br /></p>
             <p>
-                Ojuara: lá lá lá lá...<Link href={`/todos/${todoId}/comments/1`}>ver mais</Link>
+                Ojuara: lá lá lá lá...<Link href={`/todos/${todo.id}/comments/1`}>ver mais</Link>
             </p>
             <p>
-                Noé: lá lá lá lá...<Link href={`/todos/${todoId}/comments/2`}>ver mais</Link>
+                Noé: lá lá lá lá...<Link href={`/todos/${todo.id}/comments/2`}>ver mais</Link>
             </p>
         </>
     )
