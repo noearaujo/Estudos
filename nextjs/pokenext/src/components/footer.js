55beb3abc401a26c3c23e2c5007880aa31193663
noearaujo <araujonoe21@hotmail.com>
2023-04-04 06:26:06 -0300

    css inicial

--- nextjs/pokenext/src/components/footer.js
+++ nextjs/pokenext/src/components/footer.js
@@ -2,7 +2,7 @@ import styles from "@/styles/Footer.module.css"
 
 export default function Footer() {
     return (
-        <footer className={styles}>
+        <footer className={styles.footer}>
             <p>
                 <span>Noé Araújo</span> &copy; 2023
             </p>
--- nextjs/pokenext/src/components/navbar.js
+++ nextjs/pokenext/src/components/navbar.js
@@ -4,12 +4,12 @@ import styles from "@/styles/Navbar.module.css";
 
 export default function Navbar() {
     return (
-        <nav className={styles}>
-            <di>
+        <nav className={styles.navbar}>
+            <di className={styles.logo}>
                 <Image src={"/images/pokeball.png"} width={30} height={30} alt="PokeNext" />
                 <h1>PokeNext</h1>
             </di>
-            <ul>
+            <ul className={styles.menu}>
                 <li>
                     <Link href={"/"}>Home</Link>
                 </li>
