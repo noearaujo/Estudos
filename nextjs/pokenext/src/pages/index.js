5d3471072316bfea195234e3e849a2cfecd58f98
noearaujo <araujonoe21@hotmail.com>
2023-04-04 08:44:28 -0300

    chamando pokemon api

--- nextjs/pokenext/src/pages/index.js
+++ nextjs/pokenext/src/pages/index.js
@@ -1,11 +1,36 @@
 import CustomHead from "@/components/customhead"
 import styles from "@/styles/Home.module.css"
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251
+  const api = "https://pokeapi.co/api/v2/pokemon/"
+
+  const res = await fetch(`${api}?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  //add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = ++index;
+  });
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
+export default function Home({pokemons}) {
   return (
     <>
       <CustomHead title="Home"/>
-      <p>Olá</p>
+      <ul>
+        {
+          pokemons.map((pokemons) => (
+            <li key={pokemons.id}>{pokemons.name}</li>
+          ))
+        }
+      </ul>
     </>
   )
 }
